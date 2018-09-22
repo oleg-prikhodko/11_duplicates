e6c5ea431440327ac620259303fb1d2f4fc5ade8
Oleg <super.oleg.3000@yandex.ru>
2018-09-22 17:41:52 +0300

    Initial version

--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,29 @@
+import os
+import sys
+from collections import Counter, defaultdict, namedtuple
+from pprint import pprint
 
-if __name__ == '__main__':
-    pass
+
+def find_duplicates(root_path):
+    FileOnDisk = namedtuple("FileOnDisk", "name size")
+
+    files = []
+    filepaths_by_file = defaultdict(list)
+
+    for dirpath, _, filenames in os.walk(root_path):
+        for name in filenames:
+            filepath = os.path.join(dirpath, name)
+            file_on_disk = FileOnDisk(name, os.path.getsize(filepath))
+            files.append(file_on_disk)
+            filepaths_by_file[file_on_disk].append(filepath)
+
+    file_counter = Counter(files)
+    duplicates = filter(lambda item: item[1] > 1, file_counter.items())
+    return {
+        file_on_disk.name: filepaths_by_file[file_on_disk]
+        for file_on_disk, _ in duplicates
+    }
+
+
+if __name__ == "__main__":
+    pprint(find_duplicates(sys.argv[1]))

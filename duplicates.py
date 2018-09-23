aaa8c56c059619308c0305188733fcee8982f913
Oleg <super.oleg.3000@yandex.ru>
2018-09-23 11:46:37 +0300

    Simplified; name fixes

--- duplicates.py
+++ duplicates.py
@@ -1,44 +1,38 @@
 import os
 import sys
-from collections import Counter, defaultdict, namedtuple
+from collections import defaultdict, namedtuple
+
+
+# Дубликаты – это два файла с одинаковым именем и размером.
+FileOnDisk = namedtuple("FileOnDisk", "name size")
 
 
 def find_duplicates(root_path):
-    FileOnDisk = namedtuple("FileOnDisk", "name size")
-
-    files = []
-    filepaths_by_file = defaultdict(list)
+    filepaths_by_files = defaultdict(list)
 
     for dirpath, _, filenames in os.walk(root_path):
-        for name in filenames:
-            filepath = os.path.join(dirpath, name)
-            file_on_disk = FileOnDisk(name, os.path.getsize(filepath))
-            files.append(file_on_disk)
-            filepaths_by_file[file_on_disk].append(filepath)
+        for filename in filenames:
+            filepath = os.path.join(dirpath, filename)
+            file_on_disk = FileOnDisk(filename, os.path.getsize(filepath))
+            filepaths_by_files[file_on_disk].append(filepath)
 
-    file_counter = Counter(files)
-    duplicates = filter(
-        lambda frequency_by_file: frequency_by_file[1] > 1,
-        file_counter.items(),
+    duplicates = dict(
+        filter(
+            lambda filepaths_by_file: len(filepaths_by_file[1]) > 1,
+            filepaths_by_files.items(),
+        )
     )
-    return {
-        file_on_disk.name: filepaths_by_file[file_on_disk]
-        for file_on_disk, _ in duplicates
-    }
+    return duplicates
 
 
 def print_duplicates(duplicates):
-    if not duplicates:
-        print("No duplicates found")
-        return
-
-    for duplicate in duplicates.items():
+    for file_on_disk, filepaths in duplicates.items():
         print(
             "File '{}' appears {} times:".format(
-                duplicate[0], len(duplicate[1])
+                file_on_disk.name, len(filepaths)
             )
         )
-        for filepath in duplicate[1]:
+        for filepath in filepaths:
             print("\t{}".format(filepath))
 
 
@@ -51,4 +45,7 @@ if __name__ == "__main__":
         sys.exit("Input path is incorrect")
 
     duplicates = find_duplicates(root_path)
-    print_duplicates(duplicates)
+    if not duplicates:
+        print("No duplicates found")
+    else:
+        print_duplicates(duplicates)
